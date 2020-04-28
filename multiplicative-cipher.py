7f7336a5c2ab6d77e763b7574d3f54c16d6361a5
aliciacisnerosm <aliciacisnerosm@gmail.com>
2020-04-28 01:42:31 -0500

    Add decMultiplicative

--- multiplicative-cipher.py
+++ multiplicative-cipher.py
@@ -1,10 +1,22 @@
 #aliciacisneros a01281991
 english = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+import math  
 
-
+def modInverse(a, m) : 
+    a = a % m; 
+    for x in range(1, m) : 
+        if ((a * x) % m == 1) : 
+            return x 
+    return 1
+   
+# (position * key )% 26
 def encryptAlphabet(alphabet,ciphertext, key):
- posKey = (int(alphabet.find(ciphertext)) * key) % len(alphabet)
- return alphabet[posKey]
+  posKey = (int((alphabet.find(ciphertext))) * key) % len(alphabet)
+  return alphabet[posKey]
+
+def decryptAlphabet(alphabet, ciphertext, key):
+  posKey = int(alphabet.find(ciphertext)) * int(modInverse(key,len(alphabet))) % len(alphabet)
+  return alphabet[posKey]
 
 
 def multiplicative(action, key, alphabet, ciphertext):
@@ -14,8 +26,10 @@ def multiplicative(action, key, alphabet, ciphertext):
     str(x)
     if (alphabet.find(x) != -1 and action == 0):
         ciphertextAux += encryptAlphabet(alphabet,x, key)
-    else:
-        ciphertextAux += x
-  print(ciphertextAux)
+    elif(alphabet.find(x) and action == 1):
+      ciphertextAux += decryptAlphabet(alphabet, x, key)
+    else: 
+        ciphertextAux += x  
+  print (ciphertextAux)
 
-multiplicative(0, 7, english, "MAY THE FORCE BE WITH YOU")
+multiplicative(1, 7, english, "GAMDXCJUPOCHCYEDXMUK")
